f059f52301290a9f414a29e8e172a94ed1390164
Alexander Repeta <alexander.repeta@gmail.com>
2020-12-24 21:02:19 +0200

    Добавил примеры

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # html-css-30
+
+- [Вёрстка — это не тупо.](http://webmasters.teamdev.com/)
+- [Как работать с CSS-препроцессорами и БЭМ](https://nicothin.pro/idiomatic-pre-CSS/)
--- js/01-visible-password.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector(".js-password input");
-const button = document.querySelector(".js-password button");
-let isPasswordVisible = false;
-
-button.addEventListener("click", () => {
-  isPasswordVisible = !isPasswordVisible;
-  button.textContent = isPasswordVisible ? "Скрыть" : "Показать";
-  input.type = isPasswordVisible ? "text" : "password";
-});
